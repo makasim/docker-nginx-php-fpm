ab0587657ae924109ec4dd0af1b47f823bb28a1b
Andrew M-Y <andrejs@gmail.com>
2023-02-19 16:06:06 +0200

    Add multi-arch build

--- 8.2/build-images.sh
+++ 8.2/build-images.sh
@@ -3,5 +3,5 @@
 set -x
 set -e
 
-(cd 8.2/base && docker build --rm --pull -t makasim/nginx-php-fpm:8.2 -t makasim/nginx-php-fpm:latest .)
-(cd 8.2/php-all-exts && docker build --rm -t makasim/nginx-php-fpm:8.2-all-exts -t makasim/nginx-php-fpm:latest-all-exts .)
+(cd 8.2/base && docker buildx build --platform linux/amd64,linux/arm64 --rm --pull -t makasim/nginx-php-fpm:8.2 -t makasim/nginx-php-fpm:latest .)
+(cd 8.2/php-all-exts && docker buildx build --platform linux/amd64,linux/arm64 --rm -t makasim/nginx-php-fpm:8.2-all-exts -t makasim/nginx-php-fpm:latest-all-exts .)
