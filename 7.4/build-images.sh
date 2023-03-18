0b581e3e03822fe06e6a266809ca8fd5871b5732
Maksim Kotlyar <kotlyar.maksim@gmail.com>
2023-03-18 13:49:51 +0200

    fixes

--- 7.4/build-images.sh
+++ 7.4/build-images.sh
@@ -3,5 +3,5 @@
 set -x
 set -e
 
-(cd 7.4/base && docker buildx build --platform linux/amd64,linux/arm64 --rm --pull -t makasim/nginx-php-fpm:7.4 .)
-(cd 7.4/php-all-exts && docker buildx build --platform linux/amd64,linux/arm64--rm -t makasim/nginx-php-fpm:7.4-all-exts .)
+(cd 7.4/base && docker buildx build --platform linux/amd64,linux/arm64 --push --rm --pull -t makasim/nginx-php-fpm:7.4 .)
+(cd 7.4/php-all-exts && docker buildx build --platform linux/amd64,linux/arm64 --push --rm -t makasim/nginx-php-fpm:7.4-all-exts .)
--- 8.1/build-images.sh
+++ 8.1/build-images.sh
@@ -3,5 +3,5 @@
 set -x
 set -e
 
-(cd 8.1/base && docker buildx build --platform linux/amd64,linux/arm64 --rm --pull -t makasim/nginx-php-fpm:8.1 -t makasim/nginx-php-fpm:latest .)
-(cd 8.1/php-all-exts && docker buildx build --platform linux/amd64,linux/arm64 --rm -t makasim/nginx-php-fpm:8.1-all-exts -t makasim/nginx-php-fpm:latest-all-exts .)
+(cd 8.1/base && docker buildx build --platform linux/amd64,linux/arm64 --push --rm --pull -t makasim/nginx-php-fpm:8.1 -t makasim/nginx-php-fpm:latest .)
+(cd 8.1/php-all-exts && docker buildx build --platform linux/amd64,linux/arm64 --push --rm -t makasim/nginx-php-fpm:8.1-all-exts -t makasim/nginx-php-fpm:latest-all-exts .)
